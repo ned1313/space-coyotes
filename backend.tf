1d98d664a1b685c4186ab27b56eb3215f59532af
ned1313 <ned.bellavance@gmail.com>
2022-01-19 09:47:48 -0500

    first

---
+++ backend.tf
@@ -0,0 +1,20 @@
+# terraform {
+#   cloud {
+#     organization = "Terraform-Foundations-8598798c"
+
+#     workspaces {
+#       name = "space_coyote_dev"
+#     }
+#   }
+# }
+
+# Terraform Cloud Cloud Backend
+# terraform {
+#   cloud {
+#     organization = "YOURORGANIZATION"
+
+#     workspaces {
+#       name = "space_coyote_dev"
+#     }
+#   }
+# }
